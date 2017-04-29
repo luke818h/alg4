7390da98492eb724b58de8e4e3d7753c30077f8b
Luke Henwood <luke8h@gmail.com>
2017-04-28 23:09:42 -0500

    created algorithm to find the longest word in a string

--- mainehhh.js
+++ mainehhh.js
@@ -1,11 +1,10 @@
-
 function findLongestWord(str) {
   var strSplit = str.split(' ');
   var longestWord = 0;
-  for(var i = 0; i < strSplit.length; i++){
-    if(strSplit[i].length > longestWord){
-	longestWord = strSplit[i].length;
-     }
+  for (var i = 0; i < strSplit.length; i++) {
+   if (strSplit[i]).length > longestWord {
+    longestWord = strSplit[i].length;
+   }
   }
   return longestWord;
 }
